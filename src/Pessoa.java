f7b78329a13a4e5a41cd96c704bf2c075949dfa2
Caio Couto <caiocc20006@gmail.com>
2026-03-25 13:54:05 -0300

    Inicial Commit

---
+++ src/Pessoa.java
@@ -0,0 +1,72 @@
+import java.util.Date;
+
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private int idade;
+    private String cpf;
+    private Date dt_nasc;
+    private String email;
+    private String tel;
+    private String endereco;
+
+    //Construtor
+    public Pessoa() {}
+
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDt_nasc() {
+        return dt_nasc;
+    }
+
+    public void setDt_nasc(Date dt_nasc) {
+        this.dt_nasc = dt_nasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
